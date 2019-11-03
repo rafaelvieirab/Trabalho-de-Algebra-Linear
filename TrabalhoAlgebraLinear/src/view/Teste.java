85abcb3874027098cd137ce06c8523c01f075d80
natanaelvieirab <49008314+natanaelvieirab@users.noreply.github.com>
2019-11-03 20:59:12 -0300

    Inicio dos testes

---
+++ TrabalhoAlgebraLinear/src/view/Teste.java
@@ -0,0 +1,23 @@
+package view;
+
+import model.*;
+import control.*;
+
+public class Teste{
+	public static void main(String[] args) {
+		
+		float[][] valoresA = {{5,0},{7,2},{2,1}};
+		Matriz a = new Matriz(valoresA);
+		float[][] valoresB = {{2,0},{1,(-1)},{1,2}};
+		Matriz b = new Matriz(valoresB);
+		
+		Matriz r = OperationMatriz.getInstance().subMatriz(a, b);
+		
+		for(int i = 0; i<r.getLinha();i++) {
+			for(int j = 0; j<r.getColuna();j++) {
+				System.out.print(r.value(i, j));
+			}
+		}
+		
+	}
+}
