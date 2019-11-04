7ec11bcab4d867f26b6f5ed608cf3e723956fb53
rafaelvieirab <rafaelviebar@gmail.com>
2019-11-04 19:53:47 -0300

    Instalando javafx

---
+++ TrabalhoAlgebraLinear/module-info.java
@@ -0,0 +1,12 @@
+
+module Teste {
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.media;
+	requires javafx.swing;
+	requires javafx.swt;
+	requires javafx.web;
+	exports view;
+}
